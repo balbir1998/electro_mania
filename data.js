1f27d2f18718c0505c204b9b42c61338fd4ae3b4
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-05 16:04:44 +0530

    products data

---
+++ data.js
@@ -0,0 +1,62 @@
+export const productsData = [
+    {
+        src: "./assets/products/band.png",
+        title: "smart band",
+        price: 50,
+        category: "wearables"
+    },
+    {
+        src: "./assets/products/camera.png",
+        title: "digital camera",
+        price: 450,
+        category: "electronics"
+    },
+    {
+        src: "./assets/products/iphone.png",
+        title: "iphone",
+        price: 999,
+        category: "electronics"
+    },
+    {
+        src: "./assets/products/laptop.png",
+        title: "laptop",
+        price: 850,
+        category: "computers"
+    },
+    {
+        src: "./assets/products/power_bank.png",
+        title: "power bank",
+        price: 40,
+        category: "accessories"
+    },
+    {
+        src: "./assets/products/canon_dslr.png",
+        title: "canon DSLR camera",
+        price: 950,
+        category: "electronics"
+    },
+    {
+        src: "./assets/products/sound_speaker.png",
+        title: "sound speaker",
+        price: 110,
+        category: "audio"
+    },
+    {
+        src: "./assets/products/cctv.png",
+        title: "cctv camera",
+        price: 120,
+        category: "security"
+    },
+    {
+        src: "./assets/products/speaker.png",
+        title: "bluetooth speaker",
+        price: 70,
+        category: "audio"
+    },
+    {
+        src: "./assets/products/tab.png",
+        title: "tablet",
+        price: 300,
+        category: "electronics"
+    },
+];
