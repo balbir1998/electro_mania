e4f9de489f7185e819f6f073ea6bb0f5d9a144a8
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-08 15:33:59 +0530

    accessories data

---
+++ accessories.js
@@ -0,0 +1,38 @@
+export const accessoriesData = [
+    {
+        src: "./assets/products/band.png",
+        title: "smart band",
+        price: 50,
+        category: "wearables"
+    },
+    {
+        src: "./assets/products/camera.png",
+        title: "digital camera",
+        price: 450,
+        category: "electronics"
+    },
+    {
+        src: "./assets/products/power_bank.png",
+        title: "power bank",
+        price: 40,
+        category: "accessories"
+    },
+    {
+        src: "./assets/products/sound_speaker.png",
+        title: "sound speaker",
+        price: 110,
+        category: "audio"
+    },
+    {
+        src: "./assets/products/cctv.png",
+        title: "cctv camera",
+        price: 120,
+        category: "security"
+    },
+    {
+        src: "./assets/products/speaker.png",
+        title: "bluetooth speaker",
+        price: 70,
+        category: "audio"
+    },
+];
