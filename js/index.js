8c80058f9211d7458b3f6466d70a26fd6bdc6655
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-16 11:45:00 +0530

    moved renderProducts function to utils folder

--- js/index.js
+++ js/index.js
@@ -1,24 +1,9 @@
 import { productsData } from "../data/products.js";
 import { accessoriesData } from "../data/accessories.js";
+import renderProducts from './utils/renderProducts.js';
 
 const cardsContainer = document.querySelector(".cards-container");
 const accessoriesCardsContainer = document.querySelector(".accessories-cards-container");
 
 renderProducts(productsData, cardsContainer);
 renderProducts(accessoriesData, accessoriesCardsContainer)
-
-function renderProducts(data, container) {
-    let cardHtml = "";
-    data.forEach(product => {
-        cardHtml += `<a class="card" href="./product.html">
-                    <img src=${product.src} alt="card image">
-                    <p class="card-title">${product.title}</p>
-                    <span class="price">&#8377;${product.price.toLocaleString({ "en": "IN" })}</span>
-                    <div class="card-bottom">
-                        <button class="card-btn">Add To Cart</button>
-                        <button class="card-btn"><i class="fa-regular fa-heart"></i> Wishlist</button>
-                    </div>
-                 </a>`;
-    });
-    container.innerHTML = cardHtml;
-}
