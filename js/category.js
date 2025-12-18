3b6e1b12162bd5352c35dd845053740d5b5be96e
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-18 17:47:01 +0530

    added web page title dynamically

--- js/category.js
+++ js/category.js
@@ -6,5 +6,7 @@ const categoryTitle = document.querySelector(".category-title");
 const category = new URLSearchParams(location.search).get("title");
 categoryTitle.innerText = category;
 
+document.querySelector("title").innerText = `${category[0].toUpperCase() + category.slice(1)} - Electro Mania`;
+
 const filterProducts = productsData.filter(product => category.toLowerCase() === product.category.toLowerCase());
 renderProducts(filterProducts, cardsContainer);
