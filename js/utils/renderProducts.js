d89b0bf271d6ca569007a8a9b6bf0a030db986df
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-23 22:33:04 +0530

    card elements changed

--- js/utils/renderProducts.js
+++ js/utils/renderProducts.js
@@ -1,15 +1,15 @@
 export default function renderProducts(data, container) {
     let cardHtml = "";
     data.forEach(product => {
-        cardHtml += `<a class="card" href="./product.html?title=${product.title}">
+        cardHtml += `<div class="card" data-product-id=${product.id}>
                     <img src=${product.src} alt="card image">
                     <p class="card-title">${product.title}</p>
                     <span class="price">&#8377;${product.price.toLocaleString({ "en": "IN" })}</span>
                     <div class="card-bottom">
-                        <button class="card-btn">Add To Cart</button>
-                        <button class="card-btn"><i class="fa-regular fa-heart"></i> Wishlist</button>
+                        <button class="card-btn add-to-cart">Add To Cart</button>
+                        <button class="card-btn wishlist"><i class="fa-regular fa-heart"></i> Wishlist</button>
                     </div>
-                 </a>`;
+                 </div>`;
     });
     container.innerHTML = cardHtml;
 }
