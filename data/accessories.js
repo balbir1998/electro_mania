44fdce9e07aebe40c8bae1360710098400e448c4
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-23 22:06:42 +0530

    added unique id's

--- data/accessories.js
+++ data/accessories.js
@@ -1,38 +1,44 @@
 export const accessoriesData = [
     {
+        id: "Q9xF2KpLVa",
         src: "./assets/products/band.png",
         title: "smart band",
         price: 50,
         category: "wearables"
     },
     {
+        id: "M7RZP3T8wQ",
         src: "./assets/products/camera.png",
         title: "digital camera",
         price: 450,
         category: "electronics"
     },
     {
+        id: "KX5V8F9p2R",
         src: "./assets/products/power_bank.png",
         title: "power bank",
         price: 40,
         category: "accessories"
     },
     {
+        id: "P3TQ7X9F8M",
         src: "./assets/products/sound_speaker.png",
         title: "sound speaker",
         price: 110,
         category: "audio"
     },
     {
+        id: "V8R2QX7K9M",
         src: "./assets/products/cctv.png",
         title: "cctv camera",
         price: 120,
         category: "security"
     },
     {
+        id: "FQ9XK7T3P8",
         src: "./assets/products/speaker.png",
         title: "bluetooth speaker",
         price: 70,
         category: "audio"
-    },
+    }
 ];
