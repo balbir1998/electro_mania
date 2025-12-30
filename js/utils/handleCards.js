c09f72be94994d607edf1a6a10ccc2eafb142058
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-30 20:30:00 +0530

    common function to handle cards click event

---
+++ js/utils/handleCards.js
@@ -0,0 +1,41 @@
+import { cartBtn, cartData, wishlistData } from "../main.js";
+import updateLocalStorage from "./updateLocalStorage.js";
+
+export function handleCardsItems(e) {
+    if (e.target === e.currentTarget) return;
+    const productId = e.target.closest(".card").dataset['productId'];
+
+    if (e.target.classList.contains("add-to-cart")) {
+        if (cartData[productId]) {
+            if (cartData[productId].quantity >= 4) {
+                alert("Limit is 4");
+                return;
+            }
+            cartData[productId].quantity++;
+        } else {
+            cartData[productId] = { quantity: 1 };
+        };
+        updateLocalStorage("cartData", cartData);
+        alert("Product added to cart");
+        cartBtn.classList.add("active-cart");
+        document.documentElement.style.cssText = `--cart-itemsCount: "${Object.keys(cartData).length}";`
+        return;
+    }
+
+    if (e.target.classList.contains("wishlist")) {
+        if (wishlistData.includes(productId)) {
+            alert("Items is already wishlisted");
+            return;
+        }
+
+        wishlistData.push(productId);
+        updateLocalStorage("wishlistData", wishlistData);
+        alert("Item added to wishlist");
+        return;
+    };
+
+    if (e.target.classList.contains("card-bottom") ||
+        e.target.classList.contains("card")) return;
+
+    location.href = `./product.html?id=${productId}`;
+}
