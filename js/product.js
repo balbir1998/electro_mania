d82c229c86d26ea7390de78599e79ed1e9212749
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-30 20:48:56 +0530

    render product details functionality

--- js/product.js
+++ js/product.js
@@ -1,5 +1,8 @@
 import renderProducts from './utils/renderProducts.js';
 import { productsData } from './../data/products.js';
+import { cartData, wishlistData } from "../js/main.js"
+import { desktopSearchInp, responsiveSearchInp } from '../js/main.js';
+import updateLocalStorage from './utils/updateLocalStorage.js';
 const productImagesParent = document.querySelectorAll(".product-image-conatiner .product-img");
 const selectImages = document.querySelector(".images-list");
 const quantityContainer = document.querySelector(".quantity-section");
@@ -7,12 +10,49 @@ const increamentBtn = document.querySelector(".increament");
 const decreamentBtn = document.querySelector(".decreament");
 const quanityInp = document.querySelector(".quantity-section input");
 const ratings = document.querySelector(".ratings");
+const addToCartBtn = document.querySelector(".add-to-cart-btn");
+const wishlistBtn = document.querySelector(".wishlist-btn");
 const cardsContainer = document.querySelector(".cards-container");
+const productTitle = document.querySelector(".product-title");
+const productDesc = document.querySelector(".description");
+const productCategory = document.querySelector(".category");
+const productSpecs = document.querySelector(".specs");
+const productStock = document.querySelector(".availability");
+const productsPrice = document.querySelector(".product-price");
 
 renderProducts(productsData.slice(0, 5), cardsContainer);
 
-// const title = new URLSearchParams(location.search).get("title");
-// document.querySelector("title").innerText = `${title[0].toUpperCase() + title.slice(1)} - Electro Mania`;
+const searchParams = new URLSearchParams(location.search);
+const productId = searchParams.get("id");
+const isSearch = JSON.parse(searchParams.get("search"));
+
+if (productId) {
+    const product = productsData.find(product => product.id === productId);
+
+    const { images, title, description, category, specifications, price, stock } = product;
+
+    if (isSearch) {
+        desktopSearchInp.value = product.title;
+        responsiveSearchInp.value = product.title;
+    }
+
+    productImagesParent.forEach((parent, idx) => parent.children[0].src = images[idx]);
+    [...selectImages.children].forEach((img, idx) => img.src = images[idx]);
+
+    productTitle.innerText = title;
+    productDesc.innerText = description;
+    productCategory.innerHTML = `<strong>Category: </strong>${category}`;
+
+    let specsHtml = "";
+    Object.entries(specifications).forEach(([key, value]) => {
+        specsHtml += `  <li><strong>${key}:</strong> ${value}</li>`
+    });
+    productSpecs.innerHTML = specsHtml;
+    productsPrice.innerHTML = `<strong>&#8377;</strong>${price.toLocaleString("en-IN")}`;
+    productStock.innerText = stock ? "(In Stock)" : "(Out of Stock)";
+
+    document.querySelector("title").innerText = `${title[0].toUpperCase() + title.slice(1)} - Electro Mania`;
+}
 
 selectImages.addEventListener("click", (e) => {
     if (e.target === e.currentTarget) return;
@@ -66,6 +106,41 @@ quanityInp.addEventListener("blur", (e) => {
     }
 });
 
+addToCartBtn.addEventListener("click", () => {
+    const productQuantity = parseInt(quanityInp.value);
+    if (cartData[productId]) {
+
+        if (cartData[productId].quantity === 4) {
+            alert("Product count is already 4 in the cart!");
+            return;
+        }
+
+        const totalQuantity = productQuantity + cartData[productId].quantity;
+
+        if (totalQuantity > 4) {
+            alert(`Product count in cart is already ${cartData[productId].quantity}, You can only add ${4 - cartData[productId].quantity} more!`);
+            quanityInp.value = 4 - cartData[productId].quantity;
+            return;
+        }
+
+        cartData[productId].quantity = totalQuantity;
+    } else {
+        cartData[productId] = { quantity: productQuantity };
+    }
+    updateLocalStorage("cartData", cartData);
+    alert("Product is added in cart!");
+});
+
+wishlistBtn.addEventListener("click", () => {
+    if (wishlistData.includes(productId)) {
+        alert("Items is already wishlisted");
+        return;
+    }
+
+    wishlistData.push(productId);
+    updateLocalStorage("wishlistData", wishlistData);
+    alert("Item added to wishlist");
+});
 
 //on hover color stars
 ratings.addEventListener("mouseover", (e) => {
