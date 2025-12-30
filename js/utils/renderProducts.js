ff3cf8a2c9fbbf6b57e198d23d8f2ca790e5c6bd
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2025-12-30 20:31:13 +0530

    image warpped in a div

--- js/utils/renderProducts.js
+++ js/utils/renderProducts.js
@@ -2,12 +2,16 @@ export default function renderProducts(data, container) {
     let cardHtml = "";
     data.forEach(product => {
         cardHtml += `<div class="card" data-product-id=${product.id}>
-                    <img src=${product.src} alt="card image">
-                    <p class="card-title">${product.title}</p>
-                    <span class="price">&#8377;${product.price.toLocaleString({ "en": "IN" })}</span>
-                    <div class="card-bottom">
-                        <button class="card-btn add-to-cart">Add To Cart</button>
-                        <button class="card-btn wishlist"><i class="fa-regular fa-heart"></i> Wishlist</button>
+                    <div class="card-image">
+                        <img src=${product.images[0]} alt="card image">
+                    </div>
+                    <div>
+                        <p class="card-title">${product.title}</p>
+                        <span class="price">&#8377;${product.price.toLocaleString({ "en": "IN" })}</span>
+                        <div class="card-bottom">
+                            <button class="card-btn add-to-cart">Add To Cart</button>
+                            <button class="card-btn wishlist"><i class="fa-regular fa-heart"></i> Wishlist</button>
+                        </div>
                     </div>
                  </div>`;
     });
